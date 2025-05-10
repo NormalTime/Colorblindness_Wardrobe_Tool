e655e035f542f463c39029e805de6ff4a1ce5b19
Greg Piturro <113159205+NormalTime@users.noreply.github.com>
2025-05-10 12:20:37 -0400

    Update WardrobeTool.sh

--- WardrobeTool.sh
+++ WardrobeTool.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
 [ $# -ge 1 -a -f "$1" ] && input="$1" || input="-"
+
+
 shirts=()
 pants=()
-# Makes an array of arrays with three values, each sub-array storing the rgb value of a respective shirt or pair of pants
+
 while IFS= read -r line; do
         fields=($line)
         if [ ${fields[0]} = "shirt" ]; then
@@ -10,10 +13,13 @@ while IFS= read -r line; do
         else
                 pants+=( "${fields[1]} ${fields[2]} ${fields[3]}" )
         fi
+
 done < "$input"
+
 maxDistance=441.67
+
 echo "MATCHES %:"
-# Finds euclidean distance for each combination between shirts and pants in clothes.txt
+
 for item in "${shirts[@]}"; do
         for item2 in "${pants[@]}"; do
                 #calculate euclidean distance
@@ -25,15 +31,20 @@ for item in "${shirts[@]}"; do
                 x2="${fields2[0]}"
                 y2="${fields2[1]}"
                 z2="${fields2[2]}"
+
                 dx=$((x1 - x2))
                 dy=$((y1 - y2))
                 dz=$((z1 - z2))
+
                 dx2=$((dx * dx))
                 dy2=$((dy * dy))
                 dz2=$((dz * dz))
+
                 sum=$((dx2 + dy2 + dz2))
+
                 distance=$(awk "BEGIN {printf \"%.2f\", sqrt($sum)}")
-                percent=$(awk "BEGIN {printf \"%.2f\", ($distance / $maxDistance) * 100}")
+                percent=$(awk "BEGIN {printf \"%.2f\", 100 - (($distance / $maxDistance) * 100)}")
                 echo "Shirt $item and pants $item2 Match: $percent%"
+
         done
 done
